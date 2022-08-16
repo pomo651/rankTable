728e15534b9d7f039939cd6a87a41c83e288adfe
pomo651 <pomo651.io@gmail.com>
2022-08-16 11:21:33 -0700

    add files

---
+++ src/Component/LeaderBoard.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import { response } from '../response';
+import {
+  Link,
+  useParams
+} from "react-router-dom";
+
+function sortByKey(list, key) {
+  if(key === 'name') {
+    return list.sort((a, b) => a[key].localeCompare(b[key]));
+  } else {
+    return list.sort((a, b) => a[key] - b[key]);
+  }
+}
+
+function LeaderBoard(props) {
+  const { sort } = useParams();
+  let list = response.list;
+  if(sort != undefined) {
+    list = sortByKey(list, sort)
+  }
+
+  const buildContent = (list) => {
+    return (
+      list.length > 0 && list.map((item, index) => {
+        return (
+          <tr key={index}>
+            <td data-testid={`rank-${index}`}>{item.rank}</td>
+            <td data-testid={`name-${index}`}>{item.name}</td>
+            <td data-testid={`points-${index}`} className="numeric">{item.points}</td>
+            <td data-testid={`age-${index}`} className="numeric">{item.age}</td>
+          </tr>
+        )
+      })
+    )
+  }
+
+	return (
+		<div className="text-center mt-50">
+			<div>
+				<div>
+          <Link to="/rank"><button data-testid="route-rank" className='outlined' type="button">Rank</button></Link>
+          <Link to="/points"><button data-testid="route-points" className='outlined' type="button">Points</button></Link>
+		      <Link to="/name"><button data-testid="route-name" className='outlined' type="button">Name</button></Link>
+					<Link to="/age"><button data-testid="route-age" className='outlined' type="button">Age</button></Link>
+				</div>
+			</div>
+			<div className="card mx-auto pb-20 mb-30" style={{ width: '50%' }}>
+				<table className="mt-50" data-testid="app-table">
+					<thead>
+						<tr>
+							<th>Rank</th>
+							<th>Name</th>
+							<th className="numeric">Points</th>
+							<th className="numeric">Age</th>
+						</tr>
+					</thead>
+					<tbody data-testid="app-tbody">
+              {buildContent(list)}
+					</tbody>
+				</table>
+			</div>
+		</div>
+	);
+}
+
+export default LeaderBoard;
